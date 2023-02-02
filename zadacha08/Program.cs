6479afd27d7af470846b5ec5c00f715b2873c8b1
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-02 20:26:19 +0500

    zadacha15

--- zadacha08/Program.cs
+++ zadacha08/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, котораяпринимает на вход
+//  цифру, обозначающую день недели, и проверяет,
+//  является ли этот день выходным.
+
+Console.Clear();
+
+Console.WriteLine("Введите цифру дня недели a ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 6;
+int c = 7;
+if(a == b || a == c)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+ 
