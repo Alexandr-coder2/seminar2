f13e5a2275b8b49864247ca3334fb4f78fc93aa5
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-02 16:51:59 +0500

    zadacha14

---
+++ zadacha16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу которая определяет из двух чисел кратно ли 
+// одно другому.
+
+Console.Clear();
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+int b = 7;
+int c = 23;
+int res1 = a % b;
+int res2 = a % c;
+if(res1 == 0 && res2 == 0)
+{
+    Console.WriteLine("Кратно ");
+}
+else
+{
+    Console.WriteLine("Нет ");
+}
+
