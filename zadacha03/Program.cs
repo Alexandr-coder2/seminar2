455bb2054a87b7af34a6394b654d8c41060de1dc
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-02 16:02:30 +0500

    zadacha12

--- zadacha03/Program.cs
+++ zadacha03/Program.cs
@@ -1,6 +1,22 @@
-﻿//  
+﻿//  Напишите программу которая будет принимать на вход два числа
+// и выводить, является ли второе число кратным первому. Если не
+// является выводит остаток от деления.
 
 
 Console.Clear();
+
+Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+int c = a % b;
+if(c == 0 )
+{
+    Console.Write("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {c} ");
+}
 
 
--- zadacha11/Program.cs
+++ zadacha11/Program.cs
@@ -7,7 +7,11 @@ int num = new Random().Next(100, 1000);
 int a1 = num / 100;
 int a2 = num % 10;
 
-Console.WriteLine($"Дано число {num}  {a1}{a2} ");
+Console.WriteLine($"{num}");
+
+int res = a1 * 10 + a2;
+
+Console.Write($"{res}");
 
 
 
