1005e41ee109374857e69fce9d9713a35476d8b3
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-03 16:46:39 +0500

    zadacha13

--- zadacha07/Program.cs
+++ zadacha07/Program.cs
@@ -1,1 +1,30 @@
-﻿//  
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, 
+// что третьей цифры нет. 
+
+Console.Clear();
+
+Console.Write("Введите число n ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n > 99)
+{
+    while (n > 999)
+    {
+        n /= 10;
+    }
+    int res = n % 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
+
+
+
+
+
+
+
+
