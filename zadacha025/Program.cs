df32be69a457c970deab67f9207213af3f72a728
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-05 21:17:42 +0500

    zadacha025 Lekcie3

---
+++ zadacha025/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача на лекции с массивом.
+
+Console.Clear();
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+     for (int i = 0; i < arrayLength ; i++)
+     {
+        int minPosition = i;
+
+
+     }
+
+
+
+}
+
+PrintArray(arr);
+
+
+
