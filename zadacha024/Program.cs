9d06c9cd695f6d145634e121db7afb8a2f177af7
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-05 20:48:00 +0500

    zadacha024 Lekcie3

---
+++ zadacha024/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача на лекции 3 Дан текст надо поменять буквы к и с с больших
+// на маленькие.
+
+
+Console.Clear();
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие русского короля."
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}"; 
+    }
+
+    return result;
+}
+
+string newText = Replace(text, " ", "|");
+Console.WriteLine(newText);
+
+
