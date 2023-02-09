b65f2f6d53092ea3493c0c51541c365ead505d76
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-09 16:50:41 +0500

    zadacha4028

---
+++ zadacha4028/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+//  4 -> 24
+//  5 -> 120
+Console.Clear();
+
+ int Multi(int n)
+{
+    int prod = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        prod *= i;
+    }
+    return prod;   
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Multi(N)}");
+
+
+
