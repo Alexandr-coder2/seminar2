244da6c5d00e6808675e8f18738c420a97ae6698
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-09 15:48:21 +0500

    zadacha4026

---
+++ zadacha4026/Program.cs
@@ -0,0 +1,21 @@
+﻿//   Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+//   456 -> 3
+//   78 -> 2
+//   89126 -> 5
+
+int DigitCount(int A)
+{
+    int count = 0;
+    while (A!=0)
+    {
+        A /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DigitCount(N));
