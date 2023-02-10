b488f4d20ae56d4652cade7ee2bc172d8f9124d7
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-10 21:40:59 +0500

    zadacha5001

---
+++ zadacha5001/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int el in array)
+{
+    positiveSum += el > 0 ? el : 0;
+    negativeSum += el < 0 ? el : 0;
+}
+
+Console.WriteLine($"Сумма положительных чисел равна {positiveSum} Сумма отрицательных чисел равна {negativeSum}");
+
