442bf8033e6126fdbd8f61ae48d4a46f8d2daa54
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-10 19:52:40 +0500

    zadacha4029

---
+++ zadacha4029/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаётмассив из 8 элементов 
+// и выводит их на экран.
+//  
+
+
+
+Console.Clear();
+int[] GetArray()
+{
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(1, 9);
+}
+return array;
+}
+void printArray(int[] NewArray)
+{
+for (int i = 0; i < NewArray.Length; i++)
+Console.Write($"{NewArray[i]} "!);
+}
+int[] array = GetArray();
+printArray(array);
+
