db7a2cd9e2f5e5d00851206df0a22886111dc7d8
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-10 19:26:32 +0500

    zadacha4025

--- zadacha4025/Program.cs
+++ zadacha4025/Program.cs
@@ -3,6 +3,19 @@
 //  степень B.
 //    3, 5 -> 243 (3⁵)
 //    2, 4 -> 16
-Console.Clear();
+int Powe(int A, int B)
+{
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+result *= A;
+}
+return result;
+}
 
+Console.Write($"Введите A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write($"Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{a}^{b} = {Powe(a, b)}");
 
