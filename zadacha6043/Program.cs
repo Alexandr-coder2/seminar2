6e0aaf7f1eddf43eec0f31e28cd6e47024e29fd8
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-17 19:37:52 +0500

    zadacha6043

--- zadacha6043/Program.cs
+++ zadacha6043/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+double ResaltX(int b1, int k1, int b2, int k2)
+{
+    double x1 = b2 - b1;
+    double x2 = k1 - k2;
+    double x = x1 / x2;
+    return x;
+}
+
+double ResaltY(int k1, int b1, double x)
+{
+    double y = (k1 * x) + b1;
+    return y;
+}
+
+Console.Write("Введите число b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+double x = ResaltX(b1, k1, b2, k2);
+double y = ResaltY(k1, b1,x);
+Console.Write($"Координаты точки пересечения -> (x = {x}; y = {y}) ");
+
+
