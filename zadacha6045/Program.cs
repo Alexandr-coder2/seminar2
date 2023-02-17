470637f2e9872df00cfd81ef6a0a69b9ead07088
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-17 17:26:36 +0500

    zadacha6045

--- zadacha6045/Program.cs
+++ zadacha6045/Program.cs
@@ -4,4 +4,22 @@
 
 Console.Clear();
 
+int[] CopyArray(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+int[] array = { 1, 2, 3, 55 };
+
+int[] newArray = CopyArray(array);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine();
+Console.WriteLine(String.Join(", ", newArray));
+
 
