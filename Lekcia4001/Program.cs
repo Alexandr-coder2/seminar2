cf7fa5624eba837f3fce744873b1cf6caf1f774f
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-17 17:14:19 +0500

    zadacha6044

--- Lekcia4001/Program.cs
+++ Lekcia4001/Program.cs
@@ -2,5 +2,49 @@
 
 
 Console.Clear();
+string[,] table = new string[2, 5];
+// String.Empty
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4] 
+// table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4] 
 
+// table[1, 2] = "Слово";
+
+// for (int i = 0; i < 2; i++)
+// {
+// for (int j = 0; j < 5; j++)
+// {
+// Console.WriteLine($"-{table[i, j]}-");
+// }
+// }
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10); // [1, 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
 
--- zadacha6044/Program.cs
+++ zadacha6044/Program.cs
@@ -6,4 +6,18 @@
 
 Console.Clear();
 
+int[] arrayFib(int N)
+{
+    int[] array = new int[N];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
 
+Console.Write("Введите число: ");
+int nFib = int.Parse(Console.ReadLine()!);
+Console.Write(string.Join(", ", arrayFib(nFib)));
