217fd51daef8c5b5fc8df7488008bbb90f7de249
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-20 20:41:26 +0500

    zadacha7050

--- zadacha7050/Program.cs
+++ zadacha7050/Program.cs
@@ -6,7 +6,8 @@
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// 1, 7 -> такого числа в массиве нет
+// 1, 7 -> такого элемента в массиве нет
+
 
 int[,] GetArray(int row, int col, int minValue, int maxValue)
 {
@@ -33,9 +34,26 @@ void PrintArray(int[,] inArray)
     }
 }
 
+Console.WriteLine("введите первое число");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число");
+int m = int.Parse(Console.ReadLine()!);
 
+void FindElement(int[,] Array)
+{
+    if (n > Array.GetLength(0) || m > Array.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine(
+            $"значение элемента {n} строки и {m} столбца равно {Array[n - 1, m - 1]}"
+        );
+    }
+}
 
-Console.Clear();
-
+Console.WriteLine();
 int[,] myArray = GetArray(4, 3, 10, 99);
 PrintArray(myArray);
+FindElement(myArray);
