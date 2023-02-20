d2a57f7c94647df6e350a5e405f24d0889c2b2ce
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-20 20:02:52 +0500

    zadacha7052

---
+++ zadacha7050/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Clear();
+
+int[,] myArray = GetArray(4, 3, 10, 99);
+PrintArray(myArray);
---
+++ zadacha7052/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArithmeticMean(int[,] Array)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            avarage = (avarage + Array[i, j]);
+        }
+        avarage = avarage / 4;
+        Console.Write($"Среднее арифметическое равно: {avarage} ");
+    }
+}
+
+Console.Clear();
+
+Console.WriteLine();
+int[,] myArray = GetArray(4, 3, 10, 99);
+PrintArray(myArray);
+FindArithmeticMean(myArray);
