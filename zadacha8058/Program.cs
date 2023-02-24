dbe5553631e4ea065b14760ae06b5fca94386875
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-24 20:53:05 +0500

    zadacha8058

---
+++ zadacha8058/Program.cs
@@ -0,0 +1,62 @@
+﻿//  Задайте две матрицы. Напишите программу, которая будет
+//  находить произведение двух матриц.
+
+int[,] GetMatrix(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintMatrix(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplyMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    if (matrixA.GetLength(1) != matrixB.GetLength(0) )
+    {
+        Console.WriteLine("Матрицы невозможно умножить!");
+        return matrixC;
+    }
+    for (int i = 0; i < matrixC.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixC.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return matrixC;
+}
+
+Console.Clear();
+
+ 
+int[,] matrixA = GetMatrix(4, 4, -9, 9);
+int[,] matrixB = GetMatrix(4, 4, -9, 9);
+MultiplyMatrix(matrixA, matrixB);
+PrintMatrix(matrixA);
+
+Console.WriteLine();
+PrintMatrix(matrixB);
+
