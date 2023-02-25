a4e504888397438343e5eb6cdbb12f73ee610044
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-25 12:51:23 +0500

    zadacha8062

---
+++ zadacha8062/Program.cs
@@ -0,0 +1,84 @@
+﻿// Заполните спирально массив 4 на 4.
+//   1  2  3  4
+//   12 13 14 5
+//   11 16 15 6
+//   10 9  8  7
+
+int[,] GetArray2D(int m, int n)
+{
+    int[,] array2D = new int[m, n];
+    return array2D;
+}
+
+void FillArraySpirally(int[,] array2D)
+{
+    int m = array2D.GetLength(0);
+    int n = array2D.GetLength(1);
+    int k = 1;
+    bool[,] visited = new bool[m, n];
+    bool isEnd = false;
+    int x = 0, y = 0;
+    while (!isEnd)
+    {
+        isEnd = true;
+        while (y < n - 1 && !visited[x, y + 1])
+        {
+            isEnd = false;
+            array2D[x, y] = k++;
+            visited[x, y] = true;
+            ++y;
+        }
+
+        while (x < m - 1 && !visited[x + 1, y])
+        {
+            isEnd = false;
+            array2D[x, y] = k++;
+            visited[x, y] = true;
+            ++x;
+        }
+
+        while (y > 0 && !visited[x, y - 1])
+        {
+            isEnd = false;
+            array2D[x, y] = k++;
+            visited[x, y] = true;
+            --y;
+        }
+
+        while (x > 0 && !visited[x - 1, y])
+        {
+            isEnd = false;
+            array2D[x, y] = k++;
+            visited[x, y] = true;
+            --x;
+        }
+        
+        array2D[x, y] = k;
+
+    }
+}
+
+void PrintArray(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.Write($"{array2D[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+Console.Write("rows = ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("columns = ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] myArray = GetArray2D(rows, columns);
+FillArraySpirally(myArray);
+PrintArray(myArray);
+
