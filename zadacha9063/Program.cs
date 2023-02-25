1c71ee706b1d432d97041ec923d946ae33999f53
Alexandr-coder2 <spirin.alexandr@gmail.com>
2023-02-25 16:04:58 +0500

    zadacha9063

---
+++ zadacha9063/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+string NumberFor(int N)
+{
+    if(N == 1) return $"{N}";
+    return NumberFor(N - 1) + $", {N}";
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumberFor(N));
